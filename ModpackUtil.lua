a2df02a567a2c6af0cbc06a27a2d0692f6b3e19c
Dimserene <31881806+Dimserene@users.noreply.github.com>
2024-11-05 00:19:53 +0800

    Update and rename ModpackUtilFull.lua to ModpackUtil.lua

--- ModpackUtil.lua
+++ ModpackUtil.lua
@@ -3,13 +3,13 @@
 --- MOD_ID: Modpack_Util
 --- MOD_AUTHOR: [Dimserene]
 --- MOD_DESCRIPTION: Dimserene's Modpack Utility
---- VERSION: Full(Extreme)
 --- PRIORITY: -999999999999999999999999
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
 local lovely = require("lovely")
 local nativefs = require("nativefs")
+local splash_screenRef = Game.splash_screen
 
 if SMODS.Atlas then
   SMODS.Atlas({
@@ -21,7 +21,7 @@ if SMODS.Atlas then
 end
 
 
-local ModpackName = "Dimserene's Modpack - Full(Extreme)"
+local ModpackName = nativefs.read(lovely.mod_dir .. "/ModpackUtil/ModpackName.txt")
 local ModpackVersion = nativefs.read(lovely.mod_dir .. "/ModpackUtil/CurrentVersion.txt")
 local ModpackUpdate = nativefs.read(lovely.mod_dir .. "/ModpackUtil/VersionTime.txt")
 
@@ -31,7 +31,7 @@ function Game:main_menu(change_context)
 	UIBox({
 		definition = {n = G.UIT.ROOT, config = {align = "cm", colour = G.C.CLEAR}, nodes = {
 						{n= G.UIT.R, config = {align = "cr"}, nodes = {
-							{n = G.UIT.T, config = {scale = 0.3, text = ModpackName, align = "cr", colour = G.C.UI.TEXT_LIGHT}}}},
+							{n = G.UIT.T, config = {scale = 0.3, text = "Dimserene's Modpack - " .. ModpackName, align = "cr", colour = G.C.UI.TEXT_LIGHT}}}},
 						{n= G.UIT.R, config = {align = "cr", padding = 0.05}, nodes = {
 							{n = G.UIT.T, config = {scale = 0.3, text = "Current Version: " .. ModpackVersion, align = "cr", colour = G.C.UI.TEXT_LIGHT}}}},
 						{n= G.UIT.R, config = {align = "cr", padding = 0.05}, nodes = {
@@ -47,6 +47,34 @@ function Game:main_menu(change_context)
 			major = G.ROOM_ATTACH
 		}
 	})
+end
+
+local click_ref = Card.click
+function Card:click()
+  click_ref(self)
+  print(self.config.center and self.config.center.key or 'no key found')
+end
+
+function Game:splash_screen()
+    splash_screenRef(self)
+
+    SMODS.current_mod = mod
+   
+    if (SMODS.Mods["Bunco"] or {}).can_load then
+        for i = #G.P_CENTER_POOLS["Booster"], 1, -1 do
+            local entry = G.P_CENTER_POOLS["Booster"][i]
+            if string.find(entry.key, "p_bunc_virtual") then
+                table.remove(G.P_CENTER_POOLS["Booster"], i)
+            end
+        end
+
+        for i = #G.P_CENTER_POOLS["Polymino"], 1, -1 do
+            local entry = G.P_CENTER_POOLS["Polymino"][i]
+            if string.find(entry.key, "c_bunc_the") then
+                table.remove(G.P_CENTER_POOLS["Polymino"], i)
+            end
+        end
+    end
 end
 
 ----------------------------------------------
